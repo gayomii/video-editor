cc40382c2faee52d4365d836db441473f1a9cba7
gayomii <gayeon4041@gmail.com>
2024-03-09 12:45:18 +0900

    refactor: add label tag for input tag

--- video-editor/src/components/VideoPlaceholder.js
+++ video-editor/src/components/VideoPlaceholder.js
@@ -1,26 +1,24 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import video_placeholder from '../assets/video_placeholder.png';
 
 const VideoPlaceholder = ({ _onChange }) => {
-  const uploadFile = useRef();
-
   return (
     <section className={'upload-layout'}>
-      <img
-        className={'video-upload-img'}
-        src={video_placeholder}
-        alt="비디오를 업로드해주세요"
-        onClick={() => uploadFile.current.click()}
-      />
-      <input
-        className={'video-upload-input'}
-        type="file"
-        accept="video/*"
-        ref={uploadFile}
-        onChange={e => {
-          _onChange(e.target.files[0]);
-        }}
-      />
+      <label>
+        <img
+          className={'video-upload-img'}
+          src={video_placeholder}
+          alt="비디오를 업로드해주세요"
+        />
+        <input
+          className={'video-upload-input'}
+          type="file"
+          accept="video/*"
+          onChange={e => {
+            _onChange(e.target.files[0]);
+          }}
+        />
+      </label>
     </section>
   );
 };
